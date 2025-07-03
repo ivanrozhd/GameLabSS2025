ee68ab76cd363b8e471c6f775415a9c2e72fe993
Ivan Rozhdestvenskii <70483604+ivanrozhd@users.noreply.github.com>
2025-07-03 11:03:52 +0200

    fixed animation

--- Assets/Scripts/Sanity/BoostManager.cs
+++ Assets/Scripts/Sanity/BoostManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dd3383684da56aa47c4cced57f41b48c646e98f5d3503af6929080d985c88af9
+oid sha256:61f75cadf324f34df1176c976e655b39d5436a334638afb513cd28bed30925a6
 size 2807
