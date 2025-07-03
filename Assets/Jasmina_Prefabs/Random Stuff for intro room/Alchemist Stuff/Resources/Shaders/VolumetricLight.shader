9b91fbe916878feb76997ba76ff3f7ae780eb3d1
Jasmina Vulovic <vulovicjasmina@yahoo.com>
2025-07-03 11:16:14 +0200

    Added intro scene

---
+++ Assets/Jasmina_Prefabs/Random Stuff for intro room/Alchemist Stuff/Resources/Shaders/VolumetricLight.shader
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d96d325bc0159b98020069c11f69a785599be230c2b6c2cbe7fb606500f6e8d9
+size 19171
---
+++ Assets/Jasmina_Prefabs/Random Stuff for intro room/Alchemist Stuff/Scripts/VolumetricLight.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:14de337748b3ac88693382589c734c1a83fb3f22ac78bf26fd613ec8bb22613f
+size 18679
